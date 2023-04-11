fdfe0ebe183738537d96cdbae7263430533def48
Ilke11Erbil <129994698+Ilke11Erbil@users.noreply.github.com>
2023-04-11 11:27:24 +0300

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,11 @@
+from matematik import topla as toplamaIslemi
+from class import Human 
+
+import random
+
+print(toplamaIslemi(10,20))
+
+print(random.randint(0,100))
+
+human1 = Human("Mirza")
+human1.talk("Merhaba")
